4d1f36f003a9f7f99e3c4a029d00466f79bd9900
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2014-11-20 21:09:02 +0000

    Initial test of TK

--- gVPCMToVideo.py
+++ gVPCMToVideo.py
@@ -24,9 +24,9 @@
 
 import os
 import time
-import VidOverPCM
-import VideoGTSDecom
-from Tkinter import Tk, Frame, BOTH
+#import VidOverPCM
+#import VideoGTSDecom
+from Tkinter import *
 
 class ControlFrame(Frame):
 
@@ -34,18 +34,43 @@ class ControlFrame(Frame):
         Frame.__init__(self, parent, background="white")
 
         self.parent = parent
+        self.xidmlfname = StringVar()
+        self.gtsfname = StringVar()
 
         self.initUI()
+        self._addMenuBar()
+        self._addLabels()
+        self.xidmlfname.set("XidmlFname")
+        self.gtsfname.set("GTSFname")
 
     def initUI(self):
 
         self.parent.title("Simple")
-        self.pack(fill=BOTH, expand=1)
+
+    def _addMenuBar(self):
+        menubar = Menu(self.parent)
+        self.parent.config(menu=menubar)
+        fileMenu = Menu(menubar)
+        fileMenu.add_command(label="Exit", command=self.onExit)
+        menubar.add_cascade(label="File", menu=fileMenu)
+
+    def _addLabels(self):
+        xidmllabel = Label(self.parent, textvariable=self.xidmlfname)
+        gtslabel   = Label(self.parent, textvariable=self.gtsfname)
+        xidmllabel.grid(row=1,column=1)
+        gtslabel.grid(row=2,column=1)
+
+    def onExit(self):
+        exit()
 
 
 def main():
 
     root = Tk()
-    root.geometry("250x150+300+300")
-    app = Example(root)
+    root.geometry("200x400+300+300")
+    app = ControlFrame(root)
     root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
